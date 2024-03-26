d52d26578905872d26ac962f3bc5545c10822f59
Elena <i500809@student.fontys.nl>
2024-03-26 13:33:57 +0100

    connected the getexcursions and createexcursions to the frontend

---
+++ src/main/java/org/individualproject/configuration/RestCorsConfiguration.java
@@ -0,0 +1,20 @@
+package org.individualproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RestCorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
+}
